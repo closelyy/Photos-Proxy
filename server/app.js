a18f853feb31d533de8b9c8037a5a8f2d0f1dd04
Preston Whitehall <pmwhitehall@gmail.com>
2019-09-04 22:12:55 -0700

    Add component routes

--- server/app.js
+++ server/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path');
+const axios = require('axios');
+
+const reservationsProxy = 'http://localhost:3002';
+const reviewsProxy = 'http://localhost:3004';
 
 const app = express();
 const PORT = 1984;
@@ -9,6 +13,38 @@ app.use(express.static('public'));
 
 app.get('/businesses/:id', (req, res) => {
   res.sendFile(indexPath);
+});
+
+app.get('/api/reservations/', (req, res) => {
+  axios.get(`${reservationsProxy}/api/reservations`)
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((error) => {
+      res.status(404).send(error);
+    });
+});
+
+app.get('/api/reviews/:id', (req, res) => {
+  const { id } = req.params;
+  axios.get(`${reviewsProxy}/api/reviews/${id}`)
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((error) => {
+      res.status(404).send(error);
+    });
+});
+
+app.get('/businesses/Pictures/:id', (req, res) => {
+  const { id } = req.params;
+  axios.get(`${reviewsProxy}/businesses/Pictures/${id}`)
+    .then((response) => {
+      res.status(200).send(response.data);
+    })
+    .catch((error) => {
+      res.status(404).send(error);
+    });
 });
 
 app.listen(PORT, () => {
