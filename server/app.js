6265f46a306eff4bff04aeb15ff52eae146f522e
Preston Whitehall <pmwhitehall@gmail.com>
2019-09-05 11:40:45 -0700

    Update client and route ports to be consistent across project

--- server/app.js
+++ server/app.js
@@ -2,11 +2,11 @@ const express = require('express');
 const path = require('path');
 const axios = require('axios');
 
-const reservationsProxy = 'http://localhost:3002';
-const reviewsProxy = 'http://localhost:3004';
+const reviewsPort = 'http://localhost:3001';
+const reservationsPort = 'http://localhost:3002';
 
 const app = express();
-const PORT = 1984;
+const PORT = 3000;
 const indexPath = path.join(`${__dirname}/../public/index.html`);
 
 app.use(express.static('public'));
@@ -16,7 +16,7 @@ app.get('/businesses/:id', (req, res) => {
 });
 
 app.get('/api/reservations/', (req, res) => {
-  axios.get(`${reservationsProxy}/api/reservations`)
+  axios.get(`${reservationsPort}/api/reservations`)
     .then((response) => {
       res.status(200).send(response.data);
     })
@@ -27,7 +27,7 @@ app.get('/api/reservations/', (req, res) => {
 
 app.get('/api/reviews/:id', (req, res) => {
   const { id } = req.params;
-  axios.get(`${reviewsProxy}/api/reviews/${id}`)
+  axios.get(`${reviewsPort}/api/reviews/${id}`)
     .then((response) => {
       res.status(200).send(response.data);
     })
@@ -38,7 +38,7 @@ app.get('/api/reviews/:id', (req, res) => {
 
 app.get('/businesses/Pictures/:id', (req, res) => {
   const { id } = req.params;
-  axios.get(`${reviewsProxy}/businesses/Pictures/${id}`)
+  axios.get(`${reviewsPort}/businesses/Pictures/${id}`)
     .then((response) => {
       res.status(200).send(response.data);
     })
@@ -48,5 +48,5 @@ app.get('/businesses/Pictures/:id', (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log('Listening on port 1984');
+  console.log(`Listening on port ${PORT}`);
 });
